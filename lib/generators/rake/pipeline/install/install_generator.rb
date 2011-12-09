bb82ce330c2fc4cc5d918ecd140df3a5c3c6aa10
Yehuda Katz <wycats@gmail.com>
2011-12-08 17:14:55 -0800

    Add generator to nuke the asset pipeline

---
+++ lib/generators/rake/pipeline/install/install_generator.rb
@@ -0,0 +1,66 @@
+module Rake
+  class Pipeline
+    class InstallGenerator < Rails::Generators::Base
+
+      desc "Install Rake::Pipeline in this Rails app"
+
+      def disable_asset_pipeline_railtie
+        say_status :config, "Updating configuration to remove asset pipeline"
+        gsub_file app, "require 'rails/all'", <<-RUBY.strip_heredoc
+          # Pick the frameworks you want:
+          require "active_record/railtie"
+          require "action_controller/railtie"
+          require "action_mailer/railtie"
+          require "active_resource/railtie"
+          require "rails/test_unit/railtie"
+        RUBY
+      end
+
+      # TODO: Support sprockets API
+      def disable_asset_pipeline_config
+        regex = /^\n?\s*#.*\n\s*(#\s*)?config\.assets.*\n/
+        gsub_file app, regex, ''
+        gsub_file Rails.root.join("config/environments/development.rb"), regex, ''
+        gsub_file Rails.root.join("config/environments/production.rb"), regex, ''
+      end
+
+      def remove_assets_group
+        regex = /^\n(#.*\n)+group :assets.*\n(.*\n)*?end\n/
+
+        gsub_file "Gemfile", regex, ''
+      end
+
+      # TODO: Support asset-pipeline like API
+      def add_assetfile
+        create_file "Assetfile", <<-RUBY.strip_heredoc
+          # NOTE: The Assetfile will eventually be replaced with an asset-pipeline
+          # compatible API. This is mostly important so that plugins can easily
+          # inject into the pipeline.
+          #
+          # Depending on demand and how the API shakes out, we may retain the
+          # Assetfile API but pull in the information from the Rails API.
+
+          input "app/assets"
+          output "public"
+
+          match "*.js" do
+            filter Rake::Pipeline::ConcatFilter, "application.js"
+          end
+
+          match "*.css" do
+            filter Rake::Pipeline::ConcatFilter, "application.css"
+          end
+
+          # copy any remaining files
+          filter Rake::Pipeline::ConcatFilter
+        RUBY
+      end
+
+    private
+      def app
+        @app ||= Rails.root.join("config/application.rb")
+      end
+    end
+  end
+end
+
