229079b11701dead4f5cbedf8ff7189f7a0eb7a0
twinturbo <me@broadcastingadam.com>
2012-10-12 11:48:13 +0200

    Sets are faster for signifcantly large N

--- lib/rake-pipeline/filters/pipeline_finalizing_filter.rb
+++ lib/rake-pipeline/filters/pipeline_finalizing_filter.rb
@@ -1,3 +1,4 @@
+require 'set'
 module Rake
   class Pipeline
     # @private
@@ -11,8 +12,9 @@ module Rake
       # inputs to the pipeline, meaning they weren't processed
       # by any filter and should not be copied to the output.
       def input_files
-        pipeline_input_files = pipeline.input_files
-        super.reject { |file| pipeline_input_files.include?(file) }
+        pipeline_input_files = Set.new pipeline.input_files
+
+        Set.new(super) - pipeline_input_files
       end
     end
   end
