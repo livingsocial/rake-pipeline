2d907ad207b6597b6b14d5029dd245190e1acdb7
tchak <paul@chavard.net>
2012-11-01 17:58:45 +0100

    Pass project instead of assetfile to middleware

--- lib/rake-pipeline/rails_plugin.rb
+++ lib/rake-pipeline/rails_plugin.rb
@@ -3,6 +3,8 @@ require 'rake-pipeline/middleware'
 Rails.configuration.after_initialize do
   if defined?(RAKEP_ENABLED) && RAKEP_ENABLED
     assetfile = defined?(RAKEP_ASSETFILE) ? RAKEP_ASSETFILE : 'Assetfile'
-    Rails.configuration.middleware.use(Rake::Pipeline::Middleware, assetfile)
+    project = Rake::Pipeline::Project.new assetfile
+
+    Rails.configuration.middleware.use Rake::Pipeline::Middleware, project
   end
 end
